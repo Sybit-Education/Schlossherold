2fc1d4c68e9220321c7d063df5531c8590de4b6e
IdaMar <122892829+IdaMar@users.noreply.github.com>
2023-01-18 11:33:50 +0100

    article-list add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { ArticleListComponent } from './article-list/article-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ArticleListComponent},
+  {path: 'article', component: ArticleListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
