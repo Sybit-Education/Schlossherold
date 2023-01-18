76f4068d28d58d0a9ec586d0c97eccf4e6a76f3a
IdaMar <122892829+IdaMar@users.noreply.github.com>
2023-01-18 15:33:24 +0100

    add events

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { EventsComponent } from './events/events.component';
+import { ArticleComponent } from './article/article.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: ArticleListComponent},
-  {path: 'article', component: ArticleListComponent}
+  {path: 'article', component: ArticleComponent},
+  {path: 'events', component: EventsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleComponent } from './article/article.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleListComponent
+    ArticleListComponent,
+    ArticleComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
