6fbd46533d958118bd966e0bcb0dd6f2b543e516
Sommer <ase@sybit.de>
2023-01-19 15:19:05 +0100

    Markdown to html

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -24,6 +24,16 @@ export class ArticleComponent implements OnInit {
       .subscribe({
         next: (value) => {
           this.article = value;
+          if (value.fields.Text) {
+            this.article.fields.Text = this.article.fields.Text.replaceAll(/\*{1,2}(.*?)\*{1,2}/g, '<strong>$1</strong>');
+            this.article.fields.Text = this.article.fields.Text.replaceAll(/_(.*?)_/g, '<i>$1</i>');
+            this.article.fields.Text = this.article.fields.Text.replaceAll(/`(.*?)`/g, '<samp>$1</samp>');
+            this.article.fields.Text = this.article.fields.Text.replaceAll(/```(.*?)```/g, '<code>$1</code>');
+            this.article.fields.Text = this.article.fields.Text.replaceAll(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
+
+            this.article.fields.Text = this.article.fields.Text.replaceAll(/(\d\. (.+)\s)+\n*/g, '<ol>$&</ol>').replaceAll(/\d\. (.+)\s/g, '<li>$1</li>');
+            this.article.fields.Text = this.article.fields.Text.replaceAll(/(- (.+)\s)+\n*/g, '<ul>$&</ul>').replaceAll(/- (.+)\s/g, '<li>$1</li>');
+          }
         },
         error: (error) => {
           console.error(error)
